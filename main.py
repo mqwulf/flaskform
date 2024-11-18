7a71f871a3f057a7f3d7ce98d76e9a2f8143960a
Buyantogtokh Khurelbaatar <vicekurel@gmail.com>
2024-11-17 20:15:01 -0800

    updates

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def receive_data():
+    name = request.form["username"]
+    password = request.form["password"]
+    return f"<h1>Name: {name}</h1><h1>Password: {password}</h1>"
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
